a168726c6ecacea506d1c7d427ef4f087d689342
BradPohl <78940441+BradPohl@users.noreply.github.com>
2024-08-13 14:13:58 -0500

    Add files via upload

---
+++ docs/source/conf.py
@@ -0,0 +1,84 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DataflowNotebooks'
+copyright = '2024, David Koop, Colin Brown, Ramakrishna Reddy Loka, Bradley Pohlman'
+author = 'David Koop, Colin Brown, Ramakrishna Reddy Loka, Bradley Pohlman'
+release = '0.4.0-alpha.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+#reset the path
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+
+from custom_lexer import CustomPythonLexer
+
+def setup(app): #add the lexer
+    app.add_lexer('custom', CustomPythonLexer())
+
+# Use JupyterLab Pygments style
+pygments_style = 'jupyterlab_pygments.JupyterStyle'
+
+extensions = ['nbsphinx',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.viewcode']
+
+nbsphinx_execute = 'never'
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+
+# Create rules to show errors as they are shown in notebooks
+nbsphinx_prolog = """
+.. raw:: html
+    
+    <style>
+    .highlighted-note {
+        background-color: DarkSlateGray;
+        color: white;
+        padding: 5px;
+        border-radius: 4px;
+    }
+    </style>
+
+    <script>
+        $(document).ready(function() {
+            $("span.ansi-red-intense-fg.ansi-bold").each(function() {
+                $(this).closest("div.highlight").parent().css("background-color", "#fdd");
+            });
+
+            // Scroll to the element with ID based on URL hash if present
+            if (window.location.hash) {
+                var hash = window.location.hash.substring(1);
+                var element = document.getElementById(hash);
+                if (element) {
+                    element.scrollIntoView();
+                }
+            }
+
+            // Highlight <p> elements containing the text "Dataflow notes"
+            $("p").filter(function() {
+                return $(this).text().indexOf("Dataflow notes") >= 0;
+            }).addClass("highlighted-note");
+        });
+    </script>
+"""
